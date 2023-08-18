881194d6062aa451f010b7fec99738a5b2539c75
Daniel Pires <dfpires@gmail.com>
2023-08-18 19:39:55 -0300

    Add files via upload

---
+++ Lista1/src/main/java/fatecfranca/lista1/Lista1.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package fatecfranca.lista1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author daniel.pires
+ */
+public class Lista1 {
+
+    public static void main(String[] args) {
+        // cria os objetos da classe Produto
+        // () indica a chamada de um método, especial chamado construtor
+        // método tem o mesmo nome da classe 
+        Produto obj1 = new Produto(1, "bolacha", "recheada com chocolate",
+                30, false, 3.20f);
+        // toda classe em Java recebe um pacote chamado 
+        // java.lang, que possui várias classes
+        System.out.println("Id: " + obj1.id + "\n Nome " + 
+            obj1.nome + "\n Descrição " + obj1.descricao +
+            "\n Qtde: " +obj1.qtde + "\n Preço " + obj1.preco);
+        
+        Produto obj2 = new Produto(2, "sorvete", "sabor baunilha", 
+                        12, true, 6.80f);
+        // toda classe em Java recebe um pacote chamado 
+        // java.lang, que possui várias classes
+        System.out.println("Id: " + obj2.id + "\n Nome " + 
+            obj2.nome + "\n Descrição " + obj2.descricao +
+            "\n Qtde: " +obj2.qtde + "\n Preço " + obj2.preco);
+        
+        JOptionPane.showMessageDialog(null, 
+           "Id: " + obj2.id + "\n Nome " + obj2.nome + "\n Descrição " + 
+           obj2.descricao + "\n Qtde: " +obj2.qtde + "\n Preço " + obj2.preco);
+    }
+}
---
+++ Lista1/src/main/java/fatecfranca/lista1/Produto.java
@@ -0,0 +1,15 @@
+package fatecfranca.lista1;
+public class Produto {
+    int id; // int é um tipo de dado primitivo
+    String nome; // String é um tipo de dado de Classe
+    int qtde; // int é um tipo de dado primitivo
+    float preco; // float é um tipo de dado primitivo
+    boolean estoqueCritico; // boolean é um tipo de dado primitivo
+    String descricao; // String é um tipo de dado de Classe
+    // outros exemplo do tipo Classe: Integer, Float, Boolean
+
+    public Produto(int id, String nome, String descricao, 
+            int qtde, boolean estoqueCritico, float preco){
+        
+    }
+}
